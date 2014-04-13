8e68709d8ac64fabb1a75c2e22d3e08b7bd030e4
Buu Nguyen <buunguyen@gmail.com>
2014-04-13 13:09:29 -0700

    Add test stuff

---
+++ test/app.js
@@ -0,0 +1,15 @@
+require('./helper')
+var expect = require('chai').expect
+var sinon  = require('sinon')
+
+describe('app', function(){
+  var spy
+
+  beforeEach(function() {
+    spy = sinon.spy()
+  })
+
+  it('???', function(){
+    
+  })
+})
